86a29068217bf7994981a423d5e1dd2971ba1ba8
Jiwon-Choi <jwchoi.py@gmail.com>
2021-07-09 01:42:52 +0900

    2021-07-09 01:42 add shell exit

--- 3_minishell/srcs/shell.c
+++ 3_minishell/srcs/shell.c
@@ -6,7 +6,7 @@
 /*   By: jiwchoi <jiwchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 18:40:08 by jiwchoi           #+#    #+#             */
-/*   Updated: 2021/07/07 19:55:46 by jiwchoi          ###   ########.fr       */
+/*   Updated: 2021/07/09 01:40:11 by jiwchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,19 @@ int		main(void)
 	int		fildes[2];
 	int		status;
 
+	char *prompt;
+	char *line = readline(prompt);
+	char **arr = ft_split(line, ' ');
+
 	pipe(fildes);
+	if (ft_strnstr(arr[0], "exit", ft_strlen(line)))
+		exit(0);
 	pid = fork();
+
 	if (pid == 0)
 	{
-		char *prompt;
-		char *line = readline(prompt);
-		if (ft_strnstr(line, "pwd", ft_strlen(line)))
-			execve("/bin/pwd", 0, 0);
-		else if (ft_strnstr(line, "exit", ft_strlen(line)))
-			return(0);
+		if (ft_strnstr(arr[0], "pwd", ft_strlen(line)))
+			execve("/bin/pwd", arr, 0);
 		printf("%s\n", line);
 	}
 	else if (pid > 0)
