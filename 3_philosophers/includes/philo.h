7ebe4803af0fc9d7f310c5566f23ace681cf5580
Jiwon-Choi <jwchoi.py@gmail.com>
2021-09-22 12:10:43 +0900

    2021-09-22 12:10 add philosophers/

---
+++ 3_philosophers/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiwchoi <jiwchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 11:49:20 by jiwchoi           #+#    #+#             */
+/*   Updated: 2021/09/22 12:05:25 by jiwchoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h> //memset
+# include <stdio.h> //printf
+# include <sys/time.h> // gettimeofday
+# include <pthread.h>
+
+#endif
---
+++ 3_philosophers/srcs/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiwchoi <jiwchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 11:51:59 by jiwchoi           #+#    #+#             */
+/*   Updated: 2021/09/22 12:05:39 by jiwchoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+/*
+	number_of_philosophers
+	time_to_die
+	time_to_eat
+	time_to_sleep
+	number_of_times_each_philosopher_must_eat
+*/
+
+int		main(int argc, char **argv)
+{
+	
+}
